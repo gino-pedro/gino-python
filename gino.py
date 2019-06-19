f2f1ac2eaf35e36adc0b3b8e311a476c8483e149
gino-pedro <pederzolli@btinternet.com>
2019-06-19 13:59:30 +0100

    test

--- gino.py
+++ gino.py
@@ -1,6 +1,6 @@
 # test file
 
-name = ("Please Enter your name?")
+name = input("Please Enter your name?\n")
 
-print("Welcome",name,".\n")
+print("Welcome",name.upper(),".\n")
 print("This is the most basic PY file I have uploaded")
